0f426941cdda21e5bbc04332558cf8da79c2c995
ezwebcraft <real8686@gmail.com>
2017-01-05 20:54:58 -0500

    doc: update comment

--- heap_sort/heap_sort.py
+++ heap_sort/heap_sort.py
@@ -7,10 +7,15 @@ def heapsort(num_list):
   '''
  
 
+  #start is assigned the index in a of the last parent node
+
   for start in range((len(num_list)-2)/2, -1, -1):
+    
     siftdown(num_list, start, len(num_list)-1)
  
   for end in range(len(num_list)-1, 0, -1):
+  	# end represents the limit of how far down the heap to sift
     num_list[end], num_list[0] = num_list[0], num_list[end]
     siftdown(num_list, 0, end - 1)
   return num_list
+
