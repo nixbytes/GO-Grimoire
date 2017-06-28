be2196b758450f3512a614f01d8485ad551d6f9c
Elvin Ramirez <real8686@gmail.com>
2017-06-27 23:11:38 -0400

    feat: add width and height

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -7,11 +7,29 @@ Author: Elvin Ramirez
 
 '''
 
+# Basic import of game lib
+# Also add system lib for exit
+
 import pygame, sys
 
-def main():
-    return 0
+# class to handle the main function of the game
+
+class SnakeMain():
+    # parametar with = passing it as a option
+    # default will be 250 x 250
+    def __init__(self,width=250,height=250):
+        # initial the pygame
+        pygame.init()
+
+        # Self Window
+        self.width = in_Width
+        self.height = in_Height
+
+
+# def main():
+#
+#     return 0
 
 
 if __name__ == '__main__':
-    main()
+    main = SnakeMain()
