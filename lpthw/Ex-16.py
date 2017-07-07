a2640fb621ddc0c9414696f2d9f6126527ab0806
Elvin Ramirez <real8686@gmail.com>
2017-07-06 23:40:20 -0400

    feat: touch up on the print

--- lpthw/Ex-16.py
+++ lpthw/Ex-16.py
@@ -4,3 +4,9 @@ from sys import argv
 script, filename = argv
 
 print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
