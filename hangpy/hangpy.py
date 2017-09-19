3fcb873eb0c317277e8a760fc3071e7aa242d192
Elvin Ramirez <real8686@gmail.com>
2017-09-18 20:51:14 -0400

    feat: rules for individual

--- hangpy/hangpy.py
+++ hangpy/hangpy.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-from random import * 
+from random import *
 
+# create variable for then whole game
 user_score = 0
-
 pc_score = 0
 
+# simple graphic
+
+
 def hangeman(man):
-	screen = [
-	"""
+    screen = [
+        """
 	+--------+
 	|
 	|
@@ -18,27 +21,27 @@ def hangeman(man):
 	|
 	===========
 	""",
-	"""
-	+--------+
-	|        |
-	|        o
-	|
-	|
-	|
-	|
-	===========
-	""",
-	"""
+        """
 	+--------+
 	|        |
 	|        o
+	|
+	|
+	|
+	|
+	===========
+	""",
+        """
+	+--------+
+	|        |
+	|        o
 	|        |
 	|
 	|
 	|
 	===========
 	""",
-	"""
+        """
 	+--------+
 	|        |
 	|        o
@@ -48,32 +51,48 @@ def hangeman(man):
 	|
 	===========
 	""",
-	]
+    ]
 
+    print(screen[man])
+    return
 
-	print (screen[man])
-	return
 
 def start():
-    print ("Lets start to play hang py")
+    print("Lets start to play hang py")
     while game():
         pass
     score()
 
+# majority of the games function
+
+
 def game():
-	
+        #  Quickly set word for player
+    word_dictinary = ["Linux", "magic", "kitty", "playstation"]
+    word = choice(word_dictinary)
+    word_length = len(word)
+    # rules for individual
+    clue = word_length * ["_"]
+    tries = 6
+    letter_tried = ""
+    guesses = 0
+    letters_correct = 0
+    letter_wrong = 0
+    global pc_score, user_score
 
 
 def letters_guess():
-	print()
-	letter = raw_input("Take a guess at the hidden word:")
-	letter.strip()
-	letter.lower()
-	print()
-	return letter
-	
+    print()
+    letter = raw_input("Take a guess at the hidden word:")
+    letter.strip()
+    letter.lower()
+    print()
+    return letter
+
+
 def replay():
+	return
+
 
 def score():
-
-
+	return
