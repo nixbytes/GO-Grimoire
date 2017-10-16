eaf2fba7989ab64a743ab4b7a3a628f095a7cd43
Elvin Ramirez <real8686@gmail.com>
2017-10-15 20:34:56 -0400

    feat: create rect class

--- pyzero/pyzero.py
+++ pyzero/pyzero.py
@@ -14,4 +14,17 @@ WHITE_COLOR = 200, 200, 200
 
 GOLD_COLOR = 205, 145, 0
 
+'''
+make the ball object for the game
 
+using math, and tuples 
+'''
+
+ball = Rect((W/2,H/2),(30,30))
+Direction = namedtuple('Direction','x y')
+ball_dir = Direction(5,-5)
+
+bat = Rect((W/2, 0.96 * H),(120, 15))
+
+class Block(Rect):
+	def __init__ (self,colour,rect):
