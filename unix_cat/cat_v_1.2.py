ac55e6f45377396c39081868e46543ecfd297513
Elvin Ramirez <real8686@gmail.com>
2017-11-10 13:20:37 -0500

    feat: finish adding the main

--- unix_cat/cat_v_1.2.py
+++ unix_cat/cat_v_1.2.py
@@ -1,28 +1,50 @@
-#!/bin/python3
+#!/usr/local/bin/python3
 
 import sys
 from optparse import OptionParser
 
 
 class unix_cat:
-	def __init__(self):
-		self.count = 1
 
-# for line in sys.stdin:
-#     print(line, end="")
+    def __init__(self):
+        self.count = 1
 
+    def run(self,i,options):
+    	# basic mod for print the line
+    	e = ""
+    	for line in i:
+    		if options.shownend:
+    			line = line.rstrip()
+    			e = "$\n"
+    		if options.shownum:
+    			line = "{0} {1}".format(self.count, line)
+
+    		self.count += 1
+    		print(line, end=e)
 
 def main():
-	usage = "usage: %prog [option]... file..."
+    usage = "usage: %prog [option]... file..."
 
-	parser = OptionParser(usage=usage)
+    parser = OptionParser(usage=usage)
 
-	parser.add_option("-E",dest="showend",action="store_true",help="Show $ at line endings")
+    parser.add_option("-E", dest="shownend", action="store_true",
+                      help="Show $ at line endings")
 
-	parser.add_option("-n",dest="shownend",action="store_true",help="Show line numbers")
+    parser.add_option("-n", dest="shownnum",
+                      action="store_true", help="Show line numbers")
 
-	(options,args) = parser.parser_args()
+    (options, args) = parser.parse_args()
 
+    c = unix_cat()
+
+
+    # check if file name is given
+    if len(args) > 1:
+    	for a in args:
+    		f = open(a,"r")
+    		c.run(f,options)
+    else:
+    	c.run(sys.stdin, options)
 
 if __name__ == '__main__':
-	main()
+    main()
