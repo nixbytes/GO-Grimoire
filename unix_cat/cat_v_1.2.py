3b551943e380dc718fc4b92a218103bd71bde2f9
Elvin Ramirez <real8686@gmail.com>
2017-11-10 12:40:40 -0500

    feat: added the OptionParser mod for cli

--- unix_cat/cat_v_1.2.py
+++ unix_cat/cat_v_1.2.py
@@ -1,8 +1,20 @@
 #!/bin/python3
 
 import sys
+from optparse import OptionParser
+
+usage = "usage: %prog [option]... file..."
+
+parser = OptionParser(usage=usage)
+
+parser.add_option("-E",dest="showend",action="store_true",help="Show $ at line endings")
+
+parser.add_option("-n",dest="shownend",action="store_true",help="Show line numbers")
+
+
+(options,args) = parser.parser_args()
 
 for line in sys.stdin:
     print(line, end="")
 
-g
+
