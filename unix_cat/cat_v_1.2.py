cdaa191af5d2e6acb54e22b3013592aa4d393a35
Elvin Ramirez <real8686@gmail.com>
2017-11-10 12:44:38 -0500

    feat: basic def and class

--- unix_cat/cat_v_1.2.py
+++ unix_cat/cat_v_1.2.py
@@ -3,18 +3,26 @@
 import sys
 from optparse import OptionParser
 
-usage = "usage: %prog [option]... file..."
 
-parser = OptionParser(usage=usage)
+class unix_cat:
+	def __init__(self):
+		self.count = 1
 
-parser.add_option("-E",dest="showend",action="store_true",help="Show $ at line endings")
-
-parser.add_option("-n",dest="shownend",action="store_true",help="Show line numbers")
-
-
-(options,args) = parser.parser_args()
-
-for line in sys.stdin:
-    print(line, end="")
+# for line in sys.stdin:
+#     print(line, end="")
 
 
+def main():
+	usage = "usage: %prog [option]... file..."
+
+	parser = OptionParser(usage=usage)
+
+	parser.add_option("-E",dest="showend",action="store_true",help="Show $ at line endings")
+
+	parser.add_option("-n",dest="shownend",action="store_true",help="Show line numbers")
+
+	(options,args) = parser.parser_args()
+
+
+if __name__ == '__main__':
+	main()
