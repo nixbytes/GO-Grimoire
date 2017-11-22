c66d41d6bb9f629b6a0858a54fca8aa5154519f1
Elvin Ramirez <real8686@gmail.com>
2017-11-21 21:40:46 -0500

    feat: testin with pickle mod

--- PSE/feed.py
+++ PSE/feed.py
@@ -7,5 +7,12 @@ feeds = ["http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml"
 
 for feed in feeds:
     file.write("{0}\n".format(feed))
-
 file.close()
+
+
+with open("lxf-text.txt", "a") as file:
+    pickle.dump(feeds, file)
+
+
+with open("lxf-text.txt", "r") as file
+    feeds = pickle.load(file)
