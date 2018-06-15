cb9b86a0c957847e15abb664d000bdc349e7e66d
Elvin Ramirez <real8686@gmail.com>
2018-06-14 21:59:51 -0400

    working with file and permission

--- Unix_CP/cp.py
+++ Unix_CP/cp.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 import os
-
 import sys
+import stat
+from stat import *
+
 
 if len(sys.argv) >= 2:
-    what = str(sys.argv[1])
+    filename = str(sys.argv[1])
 else:
     print('Not enough arguments!!!')
     sys.exit(0)
 
-if os.path.isdir(what):
-    print(what,'is a directory')
-elif os.path.isfile(what):
-    print(what,'is a file')
+fileMetadata = [(filename, os.lstat(filename))]
+
+for name, meta in fileMetadata:
+    if S_ISDIR(meta.st_mode):
+        print('It is a directory')
+    else:
+        print('It is a regular file')
+    print(name, 'take', meta.st_size,'bytes')
+
+print('File Permission',stat.filemode(os.stat(filename).st_mode))
