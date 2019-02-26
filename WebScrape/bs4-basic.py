48a3f45f57e652968b370dbe70f99181c2265e3a
linuxbytes <real8686@gmail.com>
2019-02-25 21:34:54 -0500

    refactor

--- WebScrape/bs4-basic.py
+++ WebScrape/bs4-basic.py
@@ -1,14 +1,27 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError 
+from bs4 import BeautifulSoup
 
-try:
-	html = urlopen("http://www.pythonscraping.com/pages/page1.htmlhttp://www.pythonscraping.com/pages/page1.html")
-except HTTPError as e:
-	print(e)
-except URLError as e:
-	print("The Server could not be found!")
-else:
-	print("Its Worked")
+
+
+def getTitle(url):
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		return None
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.htmlhttp://www.pythonscraping.com/pages/page1.html")
+
+
+print(title)	
+# try:
+# 	html = urlopen("http://www.pythonscraping.com/pages/page1.htmlhttp://www.pythonscraping.com/pages/page1.html")
+# except HTTPError as e:
+# 	print(e)
+# except URLError as e:
+# 	print("The Server could not be found!")
+# else:
+# 	print("Its Worked")
 
 
