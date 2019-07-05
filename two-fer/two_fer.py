94c25f28f982ca8c4afbc960b9d7853650061fe6
linuxbytes <real8686@gmail.com>
2019-07-05 17:12:24 -0400

    create a shoter version

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,5 +1,2 @@
 def two_fer(name = "you"):
-    if name is None:
-        return "One for you, one for me."
-    else:
-        return "One for {}, one for me.".format(name)
+    return "One for {}, one for me.".format(name)
