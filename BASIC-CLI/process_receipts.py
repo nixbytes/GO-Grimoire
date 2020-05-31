e950c504846535169f1f6a9587f86787c4caea68
linuxbytes <real8686@gmail.com>
2020-05-31 19:24:49 -0400

    use built in round function to clean up the printing

--- BASIC-CLI/process_receipts.py
+++ BASIC-CLI/process_receipts.py
@@ -11,18 +11,25 @@ except OSError:
     print("'processed' directory already exists")
 
 # Get a list of receipts
-receipts = glob.glob('./new/receipt-[0-9]*.json')
+receipts = glob.glob("./new/receipt-[0-9]*.json")
 subtotal = 0.0
 
 for path in receipts:
     with open(path) as f:
         content = json.load(f)
-        subtotal += float(content['value'])
-    name = path.split('/')[-1]
-    destination = f"./processed/{name}"
+        subtotal += float(content["value"])
+
+    #    name = path.split('/')[-1]
+    #    destination = f"./processed/{name}"
+    #    shutil.move(path, destination)
+    #    print(f"moved '{path}' to '{destination}'")
+
+    # print("Receipt subtotal: $%.2f" % subtotal)
+
+    destination = path.replace("new", "processed")
+
     shutil.move(path, destination)
-    print(f"moved '{path}' to '{destination}'")
 
-print("Receipt subtotal: $%.2f" % subtotal)
+print(f"moved '{path}' to '{destination}'")
 
-
+print(f"Receipt subtotal: ${round(subtotal, 2)}")
